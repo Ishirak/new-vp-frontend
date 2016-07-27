bd57d6cbe3c02775bb90738c9d916e760e0711ed
Michal Snovický <snovickym@gmail.com>
2016-07-27 12:02:11 +0200

    Add another config files.

---
+++ src/config/config.local.sample.ts
@@ -0,0 +1,10 @@
+// Copy and update values from config.ts
+
+const config = {
+  api: {
+    root: 'http://localhost/wordpress',
+    adminUrl: 'http://localhost/wordpress/wp-admin',
+  },
+};
+
+export default config;
---
+++ src/config/config.local.ts
@@ -0,0 +1,10 @@
+// Copy and update values from config.ts
+
+const config = {
+  api: {
+    root: 'http://localhost/vp02',
+    adminUrl: 'http://localhost/vp02/wp-admin',
+  },
+};
+
+export default config;
